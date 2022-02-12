fcb897dc2592e16141af2142a1f7084f8eefac77
Bashar Jaan Khan <basharjaankhan@gmail.com>
2022-02-12 11:41:54 +0530

    Fix href warning

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   return (
     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-black">
       <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" className="flex">
+        <a href="." className="flex">
           <svg height="40" width="40">
             <defs>
               <clipPath id="cut-off-bottom">
